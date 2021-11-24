d37f6859248d0bbd078d82d9fb7158990a5a60ac
Jess Reed <jessica_reed@hms.harvard.edu>
2021-11-24 09:32:07 -0500

    User model added

--- dance_school/enrollment/models.py
+++ dance_school/enrollment/models.py
@@ -3,4 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    phone = models.CharField(max_length=32, null=True, blank=True)
+    emergency_first = models.CharField(max_length=128, null=True, blank=True)
+    emergency_last = models.CharField(max_length=128, null=True, blank=True)
+    emergency_email = models.EmailField(max_length=254, null=True, blank=True)
+    emergency_phone = models.CharField(max_length=32, null=True, blank=True)
+    accept_terms = models.BooleanField(null=True, blank=True)
+    
