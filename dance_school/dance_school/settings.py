fea9129a45e5eb7baa7268b7e49b3acd14ef8f71
Jess Reed <jessica_reed@hms.harvard.edu>
2021-11-24 10:31:47 -0500

    moving secret key to .env

--- dance_school/dance_school/settings.py
+++ dance_school/dance_school/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-***REMOVED***
+# CITATION:  Why and how to hide SECRET_KEY in Django https://www.youtube.com/watch?v=Nxa8wkELqJA
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
