95a2df9130f533c57728236ab9d32bc7d4e8a263
Jess Reed <jessica_reed@hms.harvard.edu>
2021-12-08 21:30:30 -0500

    load profile edit pseudo-form, started on save profile edits

--- dance_school/enrollment/static/enrollment/enrollment.js
+++ dance_school/enrollment/static/enrollment/enrollment.js
@@ -4,21 +4,26 @@ document.addEventListener('DOMContentLoaded', function () {
   // Confirm that we've loaded the page
   // alert('page load!');
 
-    // Add listeners to the Add to Cart buttons
-    // TO DO:  Replace them with text if registration isn't open yet
-    document.querySelectorAll('.add-button').forEach(button => {
-      button.addEventListener('click', () => addToCart(button.dataset.offering));
-    });
+  // Add listeners to the Add to Cart buttons
+  // TO DO:  Replace them with text if registration isn't open yet
+  document.querySelectorAll('.add-button').forEach(button => {
+    button.addEventListener('click', () => addToCart(button.dataset.offering));
+  });
 
-    // Add listeners to the Remove from Cart buttons
-    document.querySelectorAll('.remove-button').forEach(button => {
-      button.addEventListener('click', () => removeFromCart(button.dataset.item));
-    });
+  // Add listeners to the Remove from Cart buttons
+  document.querySelectorAll('.remove-button').forEach(button => {
+    button.addEventListener('click', () => removeFromCart(button.dataset.item));
+  });
 
-    // Add listeners to the Checkout button  (there should only be 1, but forEach handles missing buttons gracefully)
-    document.querySelectorAll('#checkout-button').forEach(button => {
-      button.addEventListener('click', () => checkout(button.dataset.order));
-    });
+  // Add listeners to the Checkout button  (there should only be 1, but forEach handles missing buttons gracefully)
+  document.querySelectorAll('#checkout-button').forEach(button => {
+    button.addEventListener('click', () => checkout(button.dataset.order));
+  });
+
+  // Add listeners to the Edit Profile button (there should only be 1, but forEach handles missing buttons gracefully)
+  document.querySelectorAll('#edit-profile-button').forEach(button => {
+    button.addEventListener('click', () => profileForm(button.dataset.profile));
+  });
 });
 
 /**
@@ -64,11 +69,11 @@ function addToCart(id) {
  * Remove a line item from the cart
  */
 
- function removeFromCart(id) {
+function removeFromCart(id) {
 
-   // Disable the button the user just clicked on, so they can't click repeatedly while waiting
-   const removeButton = document.querySelector(`.remove-button[data-item="${id}"]`);
-   removeButton.disabled = true;
+  // Disable the button the user just clicked on, so they can't click repeatedly while waiting
+  const removeButton = document.querySelector(`.remove-button[data-item="${id}"]`);
+  removeButton.disabled = true;
 
   // Add the offering to the cart via the API
   fetch(`/cart/${id}`, {
@@ -89,7 +94,7 @@ function addToCart(id) {
       removeButton.disabled = false;
     });
 
- }
+}
 
 
 
@@ -97,12 +102,12 @@ function addToCart(id) {
  * Remove a line item from the cart
  */
 
- function checkout(id) {
-   alert(id);
+function checkout(id) {
+  alert(id);
 
-    // Disable the button the user just clicked on, so they can't click repeatedly while waiting
-    const checkoutButton = document.querySelector('#checkout-button');
-    checkoutButton.disabled = true;
+  // Disable the button the user just clicked on, so they can't click repeatedly while waiting
+  const checkoutButton = document.querySelector('#checkout-button');
+  checkoutButton.disabled = true;
 
   // Submit the cart via the API
   fetch(`/checkout/${id}`, {
@@ -121,4 +126,43 @@ function addToCart(id) {
       // Reenable the add button
       checkoutButton.disabled = false;
     });
- }
+}
+
+/**
+* Replace editable profile values with form fields
+*/
+
+function profileForm(id) {
+
+  // Disable the button the user just clicked on, so they can't click repeatedly while waiting
+  const editButton = document.querySelector('#edit-profile-button');
+  editButton.disabled = true;
+
+  alert(`profileForm ${id}`);
+
+  // Replace the contents of each editable value with an input, populated with the current value
+  const fieldsList = document.querySelectorAll('.editable .value');
+  fieldsList.forEach(field => {
+    const fieldValue = field.innerHTML;
+    field.innerHTML = '';
+    const child = document.createElement('input');
+    child.id = field.id;
+    child.value = fieldValue;
+    field.appendChild(child);
+  })
+
+  // Replace the edit button with a save button
+  editButton.innerHTML = 'Save'
+  // TO DO:  Figure out how to remove existing listener
+  editButton.removeEventListener('click', profileForm)
+  editButton.addEventListener('click', () => saveProfile(id));
+  editButton.disabled = false;
+}
+
+/**
+* Save the edit profile form
+*/
+
+function saveProfile(id) {
+  alert(`saveProfile ${id}`);
+}
